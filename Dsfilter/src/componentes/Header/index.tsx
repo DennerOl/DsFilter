445bed419e07009f77754f14cbffe12bceb03e51
DennerOl <dener155@hotmail.com>
2023-12-12 23:06:36 -0300

    header

---
+++ Dsfilter/src/componentes/Header/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+export default function Header() {
+  return (
+
+    <header>
+      <div className='dsf-container dsf-container-header'>
+        <h1>DSFilter</h1>
+        <p> produto(s)</p>
+      </div>
+    </header>
+  );
+
+}
