629886f2838070865cdc400c9c6d03cdc4598e13
DennerOl <dener155@hotmail.com>
2023-12-15 22:31:42 -0300

    Tratando o form com campos não preenchidos

--- Dsfilter/src/componentes/Filter/index.tsx
+++ Dsfilter/src/componentes/Filter/index.tsx
@@ -1,23 +1,55 @@
+import { useState } from 'react';
 import './styles.css'
 
 
-export default function FilterCard() {
+type FormData = {
+  minimo?: number,
+  maximo?: number;
+
+}
+
+export default function Filter() {
+
+  const [formData, setFormData] = useState<FormData>({
+    minimo: undefined,
+    maximo: undefined
+
+  });
+
+  function handleInputChange(event: any) {
+    const value = event.target.value;
+    const name = event.target.name;
+    setFormData({ ...formData, [name]: value });
+  }
+
+  function handleSubmit(event: any) {
+    event.preventDefault();
+    (formData.minimo || Number.MIN_VALUE),
+      (formData.maximo || Number.MAX_VALUE);
+
+
+  }
 
   return (
     <div className='card '>
       <div className=' dsf-filter-card dsf-mb20  dsf-container  '>
         <div className=''>
 
-          <form className='dsf-form'>
+          <form onSubmit={handleSubmit} className='dsf-form'>
             <input
+              name="minimo"
+              value={formData.minimo || ""}
               type="text"
               placeholder='Preço mínimo'
-              name='PrecoMin'
+              onChange={handleInputChange}
             />
 
-            <input type="text"
+            <input
+              name="maximo"
+              value={formData.minimo || ""}
+              type="text"
               placeholder='Preço máximo'
-              name='PrecoMax'
+              onChange={handleInputChange}
             />
 
             <button className='dsf-button'> Filtrar</button>
